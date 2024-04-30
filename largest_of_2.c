f57e27ddcd85dbe23ed923ce013dabb05ba5c31f
Nikhil Madaravena <nikhil.madaravena@gmail.com>
2024-04-30 22:07:11 +0530

    operators

---
+++ largest_of_2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	scanf("%d%d",&a,&b);
+	if(a>b)
+	printf("largest = %d\n",a);
+	else
+	printf("largest = %d\n",b);
+}
---
+++ relational_operators.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("Enter a value: ");
+	scanf("%d",&a);
+	printf("Enter b value: ");
+	scanf("%d",&b);
+	
+	printf("A<=B: %d\n",a<=b);
+	printf("A>B: %d\n",a>b);
+	printf("A!=B: %d\n",a!=b);
+}
